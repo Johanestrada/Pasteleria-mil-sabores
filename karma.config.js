900802bf60347e3760801280344e3ab0d1998c16
Johan Estrada <johan.manuel.estrada.plaza@gmail.com>
2025-10-23 17:48:49 -0300

    karma.conf - Jasmine
    Se arregló archivo karma.config.js

--- karma.config.js
+++ karma.config.js
@@ -1,36 +1,50 @@
-const { web } = require('webpack');
-const webpack = require('./webpack.config.js');
+const webpackConfig = require('./webpack.config.js');
+
 module.exports = function(config) {
-  config.set({
-    frameworks: ['jasmine'],
+    config.set({
+        //Framework base
+        framework: ['jasmine'],
 
-    files: [
-      'src/**/*.test.js',
-      'src/**/*.test.jsx',
-      'src/**/*.test.ts',
-      'src/**/*.test.tsx'
-    ],
-    preprocessors: {
-      'src/**/*.test.js': ['webpack'],
-      'src/**/*.test.jsx': ['webpack'],
-      'src/**/*.test.ts': ['webpack'],
-      'src/**/*.test.tsx': ['webpack']
-    },
+        //Archivos a incluir en las pruebas
+        files: [
+            'src/**/*.test.js',
+            'src/**/*.test.jsx',
+            'src/**/*.test.ts',
+            'src/**/*.test.tsx'
+        ],
 
-    webpack: {
-        ...webpack.config,
-        mode: 'development',
-        resolve: {
-            ...webpack.config.resolve.alias: {
-                ...webpack.config.resolve.alias,
-    },
+        //Preprocesadores
+        preprocessors: {
+            'src/**/*.test.js' : ['webpack'],
+            'src/**/*.test.jsx' : ['webpack'],
+            'src/**/*.test.ts' : ['webpack'],
+            'src/**/*.test.tsx' : ['webpack'],
+        },
 
-    //Navegadores
-    browsers:[' chrome'],
-    reporters: ['progress'],
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    singleRun: proccess.env.CI === 'true',
-    concurrency: Infinity,
-  });
+        //Configuración de Webpack
+        webpack: {
+            ...webpackConfig,
+            mode: 'development',
+            resolve: {
+                ...webpackConfig.resolve,
+                alias: {
+                    ...webpackConfig.resolve.alias,
+                }
+            }
+        },
+
+        //Navegadores
+        browsers: [ 'Chrome '],
+        reporters: ['progress'],
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        singleRun: process.env.CI === 'true',
+        concurrency: Infinity,
+        plugins: [
+            'karma-jasmine',
+            'karma-chrome-launcher',
+            'karma-webpack'
+        ]
+
+    })
 }
